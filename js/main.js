e858318f142a6b5a2eafd9bcd82c401f5849e9a7
tomohiro-yoshida20 <tomocha.ty.20@gmail.com>
2021-01-17 14:27:40 +0900

    jSの追加 css調整

--- js/main.js
+++ js/main.js
@@ -1,9 +1,47 @@
 'use strict';
 {
+  // hamberger
+  const hamberger = document.getElementById('btn01');
+  // header
+  const header = document.getElementById('header');
+  // modal
+  const modal = document.getElementById('modal');
 
-
-
-
-
+  // hamberger-menu クリック
+  hamberger.addEventListener('click', () => {
+    hamberger.classList.add('active');
+    // header ウィンドウ上部へ隠す
+    header.classList.add('active');
+    // modal ウィンドウ上部からおろす
+    modal.classList.add('active');
+  });
   
+  // modal のどこかをクリック
+  modal.addEventListener('click', () => {
+    hamberger.classList.remove('active');
+    // header を下す
+    header.classList.remove('active');
+    // modal 戻す
+    modal.classList.remove('active');
+  });
+
+  // typing
+  const typing = (element, sentence) => {
+    [...sentence].forEach((letter, index) => {
+    // 1文字を0.1秒毎にずらして出力
+      setTimeout(() => {
+        document.querySelector(element).textContent += letter;
+      }, 100 * ++index);
+    });
+  }
+
+  // メインタイトル文字
+  const titleLtter = "Tomohiro Yoshida 's" + "\n" + "Portforio Site";
+  // サブタイトル文字
+  const subLetter = 'Web Design & Engineering';
+
+  // メインタイトルを出力
+  typing('#title', titleLtter);
+  // メインタイトル出力後サブを出力
+  setTimeout(() => {typing('#title-sub', subLetter)}, titleLtter.length * 100);
 }
